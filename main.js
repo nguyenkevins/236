d80412848661ffe90ba91c6dab3ce355a584642a
huyn8 <huyn8@uw.edu>
2019-08-14 02:01:18 -0700

    new updates

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ function myFunction5() {
     document.getElementById("myDropdown5").classList.toggle("show");
 }
 
+function myFunction6() {
+    document.getElementById("myDropdown6").classList.toggle("show");
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
@@ -31,5 +35,22 @@ window.onclick = function (event) {
             }
         }
     }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
